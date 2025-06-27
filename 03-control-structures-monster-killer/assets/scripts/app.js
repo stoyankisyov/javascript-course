97bbf92fef9fd55e184157d4c0a816e48ac16dff
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-06-27 21:05:58 +0300

    Convert to switch-case

--- 03-control-structures-monster-killer/assets/scripts/app.js
+++ 03-control-structures-monster-killer/assets/scripts/app.js
@@ -35,14 +35,21 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
     finalPlayerHealth: playerHealth,
   };
 
-  if (event === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry.target = 'PLAYER';
-  } else if (event === LOG_EVENT_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
+    default:
+      logEntry = {};
   }
 
   battleLog.push(logEntry);
