f4a41b659f475e70a3a379a3fd284bd25cb511a5
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-07-05 17:46:52 +0300

    Accept user input

--- 04-functions/app.js
+++ 04-functions/app.js
@@ -1,5 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getUserChoise = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid input, ${DEFAULT_USER_CHOICE} was selected for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+
+  return selection;
+};
+
 startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game started!');
+  const userChoice = getUserChoise();
+  console.log(`User choice: ${userChoice}`);
 });
