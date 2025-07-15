3be6db8a5cd360d329efd2bec6ba347c7a811ba2
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-07-15 12:02:46 +0300

    Open and close add modal

--- 05-dom/assets/scripts/app.js
+++ 05-dom/assets/scripts/app.js
@@ -1,5 +1,31 @@
 const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
+
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+
+const backdrop = document.getElementById('backdrop');
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelMovieModal = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelMovieModal);
