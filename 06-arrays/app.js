8318b4e8849384b200062bf9df2947710adcd992
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-07-23 16:06:34 +0300

    Ways to create an array

---
+++ 06-arrays/app.js
@@ -0,0 +1,26 @@
+const numbers = [1, 2, 3];
+console.log('[]', numbers);
+
+const emptyArray = Array(5);
+console.log('Array(5)', emptyArray);
+
+const moreNumbers = Array(5, 2);
+console.log('Array(5, 2)', moreNumbers);
+
+const yetMoreNumbers = Array.of(1, 2);
+console.log('Array.of(1, 2)', yetMoreNumbers);
+
+const listItems = document.querySelectorAll('li');
+console.log('NodeList', listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log('Array.from(NodeList)', arrayListItems);
+
+const personalData = [30, 'Max', {moreDetail: []}];
+console.log('Different types', personalData);
+
+const analyticsData = [
+  [1, 1.6],
+  [-5.4, 2.1],
+];
+console.log('Nested arrays', analyticsData);
