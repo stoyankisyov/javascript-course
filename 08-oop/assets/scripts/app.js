7af789ee416c7fb2018375c99234fe3c3f028288
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-08-26 19:59:57 +0300

    Use multiple classes to render a product

--- 08-oop/assets/scripts/app.js
+++ 08-oop/assets/scripts/app.js
@@ -1,50 +1,68 @@
 class Product {
-  constructor(title, image, desc, price) {
-    this.title = title;
-    this.imageUrl = image;
-    this.description = desc;
-    this.price = price;
-  }
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
 }
 
-const productList = {
-  products: [
-    new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
-      89.99
-    )
-  ],
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
 
-    for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
         <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}" >
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
           <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
             <button>Add to Cart</button>
           </div>
         </div>
       `;
-      prodList.append(prodEl);
-    }
-    renderHook.append(prodList);
-  }
-};
 
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        new Product(
+            'A Pillow',
+            'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+            'A soft pillow!',
+            19.99
+        ),
+        new Product(
+            'A Carpet',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+            'A carpet which you might like - or not.',
+            89.99
+        )];
+
+    constructor() {
+    }
+
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+
+        for (const prod of this.products) {
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
+            prodList.append(prodEl);
+        }
+
+        renderHook.append(prodList);
+    }
+}
+
+const productList = new ProductList();
 productList.render();
