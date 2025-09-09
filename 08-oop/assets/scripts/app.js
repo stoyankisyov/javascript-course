997c8924d4a8c7a7f5fac608663ea8692187ae9b
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-09 18:53:19 +0300

    Calculate total amount on product added to cart

--- 08-oop/assets/scripts/app.js
+++ 08-oop/assets/scripts/app.js
@@ -10,9 +10,24 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  get totalAmount() {
+    const sum = this.items.reduce((prevValue, curItem) => {
+      return prevValue + curItem.price;
+    }, 0);
+    return sum;
+  }
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
   addProduct(product) {
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
