b162afe2aee3212ab3a635e28192fd204e0d5bc8
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-14 16:55:02 +0300

    Pure and impure functions

---
+++ 12-advanced-functions/app.js
@@ -0,0 +1,13 @@
+// Pure Function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(2, 3));
+
+// Impure Function
+function addRandom(num1) {
+  return num1 + Math.random();
+}
+
+console.log(addRandom(5));
