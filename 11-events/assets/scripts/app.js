270b125e3d8de601ca1e70fa7f08760f884aae32
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-14 16:39:06 +0300

    Drag and drop projects

--- 11-events/assets/scripts/app.js
+++ 11-events/assets/scripts/app.js
@@ -85,6 +85,7 @@ class ProjectItem {
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton(type);
+    this.connectDrag();
   }
 
   connectMoreInfoButton() {
@@ -107,6 +108,13 @@ class ProjectItem {
       'click',
       this.updateProjectListsHandler.bind(null, this.id)
     );
+  }
+
+  connectDrag() {
+    document.getElementById(this.id).addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', this.id);
+      event.dataTransfer.effectAllowed = 'move';
+    });
   }
 
   update(updateProjectListsFn, newType) {
@@ -144,6 +152,44 @@ class ProjectList {
         new ProjectItem(item.id, this.switchProject.bind(this), this.type)
       );
     }
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+
+    list.addEventListener('dragenter', (event) => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+      }
+      list.parentElement.classList.add('droppable');
+    });
+
+    list.addEventListener('dragover', (event) => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener('dragleave', (event) => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove('droppable');
+      }
+    });
+
+    list.addEventListener('drop', (event) => {
+      const projectId = event.dataTransfer.getData('text/plain');
+      if (this.projects.find((p) => p.id === projectId)) {
+        return;
+      }
+
+      document
+        .getElementById(projectId)
+        .querySelector('button:last-of-type')
+        .click();
+      list.parentElement.classList.remove('droppable');
+      event.preventDefault();
+    });
   }
 
   setSwitchHandlerFunction(switchHandler) {
@@ -177,9 +223,11 @@ class App {
 
     const timerId = setTimeout(this.startAnalytics, 3000);
 
-    document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-      clearTimeout(timerId);
-    });
+    document
+      .getElementById('stop-analytics-btn')
+      .addEventListener('click', () => {
+        clearTimeout(timerId);
+      });
   }
 
   static startAnalytics() {
