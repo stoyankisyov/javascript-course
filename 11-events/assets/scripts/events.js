3075611bb6a111a421145f625c60f028bc2ebc61
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-14 12:24:42 +0300

    Add mouseover event

--- 11-events/assets/scripts/events.js
+++ 11-events/assets/scripts/events.js
@@ -1,13 +1,19 @@
 const button = document.querySelector('button');
+const button2 = document.getElementById('button2');
 
 const onButtonClick = () => {
   alert('You clicked the button!');
 };
 
+const onButton2Hover = (event) => {
+  console.log(event);
+};
+
 const boundFn = onButtonClick.bind(this);
 
 button.addEventListener('click', boundFn);
+button2.addEventListener('mouseover', onButton2Hover);
 
 setTimeout(() => {
-  button.removeEventListener('click', boundFn);
+  button.removeEventListener('click', onButtonClick);
 }, 5000);
