b6971530564114c6c73d9898b2401d41883e44ea
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-14 12:58:51 +0300

    Use event delegation

--- 11-events/assets/scripts/events.js
+++ 11-events/assets/scripts/events.js
@@ -1,29 +1,40 @@
 const button = document.querySelector('button');
 const button2 = document.getElementById('button2');
 const form = document.querySelector('form');
+const div = document.querySelector('div');
+const list = document.querySelector('ul');
 
-const onButtonClick = () => {
-  alert('You clicked the button!');
-};
-
-const onButton2Hover = (event) => {
+const onButtonEvent = (event, message) => {
+  console.log(message);
   console.log(event);
 };
 
-const boundFn = onButtonClick.bind(this);
+const boundFn = onButtonEvent.bind(this, 'Button clicked');
 
 button.addEventListener('click', boundFn);
-button2.addEventListener('mouseover', onButton2Hover);
+button2.addEventListener('mouseover', (event) =>
+  onButtonEvent(event, 'Hovering')
+);
 
-window.addEventListener('scroll', (event) => {
-  console.log(event);
-});
+window.addEventListener('scroll', (event) => onButtonEvent(event, 'Scrolling'));
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log(event);
 });
 
+div.addEventListener(
+  'click',
+  (event) => {
+    onButtonEvent(event, 'Div clicked');
+  },
+  true // Capturing phase
+);
+
+list.addEventListener('click', (event) => {
+    event.target.classList.toggle('highlight');
+});
+
 setTimeout(() => {
-  button.removeEventListener('click', onButtonClick);
+  button.removeEventListener('click', boundFn);
 }, 5000);
