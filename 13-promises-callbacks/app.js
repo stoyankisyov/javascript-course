39a45fffc832a9d6fcdd46a6df5003af1eb51d41
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-15 12:07:23 +0300

    Convert to async/await

--- 13-promises-callbacks/app.js
+++ 13-promises-callbacks/app.js
@@ -26,23 +26,10 @@ const setTimer = (duration) => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-    .then((posData) => {
-      positionData = posData;
-      return setTimer(2000);
-    })
-    .then((data) => {
-      console.log(data, positionData);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  });
-  console.log('Getting position...');
+async function trackUserHandler() {
+  const posData = await getPosition();
+  const timerData = await setTimer(2000);
+  console.log(timerData, posData);
 }
 
 button.addEventListener('click', trackUserHandler);
