3c224d168e1c43d52b37a07d6f8b377f975d45e9
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-15 11:37:42 +0300

    Promises and promises chaining

--- 13-promises-callbacks/app.js
+++ 13-promises-callbacks/app.js
@@ -1,27 +1,41 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-const setTimer = duration => {
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('Done!');
     }, duration);
   });
-  
+
   return promise;
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimer(2000).then(data => {
-        console.log(data, posData);
-      });
-    },
-    error => {
-      console.log(error);
-    }
-  );
+  let positionData;
+  getPosition()
+    .then((posData) => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    });
   setTimer(1000).then(() => {
     console.log('Timer done!');
   });
