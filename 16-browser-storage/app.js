0ab8948adf86b9a72e54087d617ef354d37b213a
Stoyan Kisyov <stoyan.r.kisyov@gmail.com>
2025-09-18 14:38:39 +0300

    Local storage

---
+++ 16-browser-storage/app.js
@@ -0,0 +1,29 @@
+const storeButton = document.getElementById('store-btn');
+const retrieveButton = document.getElementById('retrieve-btn');
+
+const userId = 'user123';
+const user = {
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+};
+
+storeButton.addEventListener('click', () => {
+  localStorage.setItem('userId', userId);
+  localStorage.setItem('user', JSON.stringify(user));
+});
+
+retrieveButton.addEventListener('click', () => {
+  const storedUserId = localStorage.getItem('userId');
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  if (storedUserId) {
+    console.log(`Retrieved User ID: ${storedUserId}`);
+  } else {
+    console.log('No User ID found in localStorage.');
+  }
+
+  if (storedUser) {
+    console.log(`Retrieved User: ${JSON.stringify(storedUser)}`);
+  } else {
+    console.log('No User found in localStorage.');
+  }
+});
